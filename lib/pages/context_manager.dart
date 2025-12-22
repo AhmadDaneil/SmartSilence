50d3e860b604d56655a5a1247552a01c1845d963
Ahmad Daneil <adwari132@gmail.com>
2025-12-22 20:14:20 +0800

    Complete the "Context_Manager"

--- lib/pages/context_manager.dart
+++ lib/pages/context_manager.dart
@@ -1,15 +1,47 @@
 import 'package:flutter/material.dart';
 
-class ContextManager extends StatefulWidget {
+class ContextManager extends StatelessWidget {
   const ContextManager({super.key});
 
   @override
-  State<ContextManager> createState() => _ContextManagerState();
-}
-
-class _ContextManagerState extends State<ContextManager> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Context Manager")
+      ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: (){}, 
+          icon : const Icon(Icons.add_location_alt),
+          label: const Text("Add Context"), 
+          ),
+      body: ListView(
+        padding: const EdgeInsets.all(16),
+        children: [
+          const Text("Saved Locations", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+          const SizedBox(height: 10),
+          _buildContextCard("University Library", "Radius: 50m", Icons.local_library, true),
+          _buildContextCard("Lecture Hall", "Radius: 30m", Icons.class_, false),
+
+          const Divider(height: 40,),
+
+          const Text("Saved Wi-Fi Networks", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+          const SizedBox(height: 10),
+          _buildContextCard("UiTM Wifi", "SSID Match", Icons.wifi, true),
+          _buildContextCard("MyHome Wifi", "SSID Match", Icons.home, false),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildContextCard(String title, String subtitle, IconData icon, bool isActive) {
+    return Card(
+      margin: const EdgeInsets.only(bottom: 12),
+      child: ListTile(
+        leading: CircleAvatar(child: Icon(icon)),
+        title: Text(title),
+        subtitle: Text(subtitle),
+        trailing: Switch(value: isActive, onChanged: (val) {}),
+      ),
+    );
   }
 }
--- lib/pages/smart_insight.dart
+++ lib/pages/smart_insight.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 
-class SmartInsight extends StatefulWidget {
+class SmartInsight extends StatelessWidget {
   const SmartInsight({super.key});
 
-  @override
-  State<SmartInsight> createState() => _SmartInsightState();
-}
-
-class _SmartInsightState extends State<SmartInsight> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
